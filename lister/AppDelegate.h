4d62b0313f4644fe31578461ade2ddb10b3ba09d
Geoff Cornwall <ge0ff@ge0ff.com>
2013-07-31 13:59:55 -0400

    Initial Commit

---
+++ lister/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  lister
+//
+//  Created by Geoff Cornwall on 7/31/13.
+//  Copyright (c) 2013 Geoff Cornwall. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
