9f972d21ac3814a731b124680ee09620a7fe5f20
Geoff Cornwall <ge0ff@ge0ff.com>
2013-08-20 10:50:54 -0400

    Crashlytics integration

--- lister/LTRAppDelegate.m
+++ lister/LTRAppDelegate.m
@@ -8,13 +8,15 @@
 
 #import "LTRAppDelegate.h"
 #import "LTRListsViewController.h"
+#import <Crashlytics/Crashlytics.h>
 
 @implementation LTRAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     
-    // TestFlight integration
+    // TestFlight and Crashlytics integration
     [TestFlight takeOff:@"73ed7ed8-c4f4-4433-9c33-1d4c980ddbc4"];
+    [Crashlytics startWithAPIKey:@"c5a1a833f477c2c1bc556289437cee0418ada93d"];
     
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
 
