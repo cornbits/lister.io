614c276f13152b9b240129c93dc00e12fe26b77d
Geoff Cornwall <ge0ff@ge0ff.com>
2013-10-01 23:34:34 -0400

    Fixed random color utility

--- lister/LTRUtility.m
+++ lister/LTRUtility.m
@@ -11,12 +11,9 @@
 @implementation LTRUtility
 
 + (UIColor *)randomColor {
-//    CGFloat hue = (arc4random() % 128 / 256.0) + 0.33;
-//    CGFloat saturation = (arc4random() % 128 / 256.0) + 0.5;
-//    CGFloat brightness = (arc4random() % 128 / 256.0) + 0.5;
-    CGFloat hue = ( arc4random() % 256 / 256.0 );  //  0.0 to 1.0
-    CGFloat saturation = ( arc4random() % 128 / 256.0 ) + 0.5;  //  0.5 to 1.0, away from white
-    CGFloat brightness = ( arc4random() % 128 / 256.0 ) + 0.5;  //  0.5 to 1.0, away from black
+    CGFloat hue = ( arc4random() % 256 / 256.0 );
+    CGFloat saturation = ( arc4random() % 128 / 256.0 ) + 0.66;
+    CGFloat brightness = ( arc4random() % 128 / 256.0 ) + 0.33;
     return [UIColor colorWithHue:hue saturation:saturation brightness:brightness alpha:1];
 }
 
